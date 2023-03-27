587a3fad4b423d8beec0c77a1a415fe0a50652b3
catnaut <catnaut@outlook.com>
2023-03-27 14:04:03 +0800

    test(LinkStack): 增加 main.c
    这个比较简单，暂时不写 menu 了

--- Week_2/LinkStack/Sources/main.c
+++ Week_2/LinkStack/Sources/main.c
@@ -1,1 +1,28 @@
- 
+#include <stdio.h>
+#include "LinkStack.h"
+
+int main(int argc, char const *argv[])
+{
+    LinkStack stack;
+    initLStack(&stack);
+    printf("status:%d\n",isEmptyLStack(&stack));
+    int a;
+    printf("status:%d\n",getTopLStack(&stack,&a));
+    printf("a:%d\n",a);
+    printf("status:%d\n",pushLStack(&stack,1));
+    printf("status:%d\n",getTopLStack(&stack,&a));
+    printf("a:%d\n",a);
+    printf("status:%d\n",pushLStack(&stack,2));
+    printf("status:%d\n",getTopLStack(&stack,&a));
+    printf("status:%d\n",popLStack(&stack,&a));
+    printf("a:%d\n",a);
+    printf("status:%d\n",getTopLStack(&stack,&a));
+    printf("a:%d\n",a);
+    printf("status:%d\n",clearLStack(&stack));
+    printf("status:%d\n",getTopLStack(&stack,&a));
+    printf("a:%d\n",a);
+    printf("status:%d\n",pushLStack(&stack,3));
+    printf("status:%d\n",destroyLStack(&stack));
+    printf("status:%d\n",getTopLStack(&stack,&a));
+    return 0;
+}
